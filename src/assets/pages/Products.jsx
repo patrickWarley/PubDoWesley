672b7b9bbaa250cd71809cf004a4cc24438d28cb
Wesley Gomes <wesley.gomes222@hotmail.com>
2023-01-28 20:07:03 -0300

    adicionando os produtos na página

--- src/assets/pages/Products.jsx
+++ src/assets/pages/Products.jsx
@@ -31,6 +31,85 @@ function Products(){
         .then(response => setRefeicoes(response.data))
         .catch(error => console.error(error))
     }, [])
+
+    return(
+        <div className="produtos">
+          <div>
+      <h2 className="tituloProduto" id="produtos">Cervejas</h2>
+      <div >
+        {cervejas ? (
+
+          <div className='content product-grid'>
+
+            {cervejas.map(cerveja => (
+              <div className="product-card" key={cerveja.id}>
+                <div className="card-img">
+                  <img src={cerveja.imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{cerveja.nome}</h3>
+                  <p>Unidade: {cerveja.unidade}</p>
+                  <p className="price">Preço: {cerveja.preco}</p>
+                  <button>Adicionar ao carrinho</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Carregando...</div>
+        )}
+      </div>
+      </div>
+      <h2 className="tituloProduto">Destilados</h2>
+      <div >
+        {destilados ? (
+
+          <div className='content product-grid'>
+
+            {destilados.map(destilado => (
+              <div className="product-card" key={destilado.id}>
+                <div className="card-img">
+                  <img src={destilado.imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{destilado.nome}</h3>
+                  <p>Unidade: {destilado.unidade}</p>
+                  <p className="price">Preço: {destilado.preco}</p>
+                  <button>Adicionar ao carrinho</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Carregando...</div>
+        )}
+      </div>
+      <h2 className="tituloProduto">Drinks</h2>
+      <div >
+        {coqueteis ? (
+
+          <div className='content product-grid'>
+
+            {coqueteis.map(coquetel => (
+              <div className="product-card" key={coquetel.id}>
+                <div className="card-img">
+                  <img src={coquetel.imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{coquetel.nome}</h3>
+                  <p>Unidade: {coquetel.unidade}</p>
+                  <p className="price">Preço: {coquetel.preco}</p>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Carregando...</div>
+        )}
+      </div>
+    </div>    
+    );
 }
 
 export default Products
