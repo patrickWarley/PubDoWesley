31289e239ad332aeecff2b8fdc258493eda8ad98
patrickwarley <patrickwarley@gmail.com>
2023-01-29 19:21:16 -0300

    Ajutes nos estilos da pagina de produto

--- src/assets/pages/Products.jsx
+++ src/assets/pages/Products.jsx
@@ -33,83 +33,85 @@ function Products() {
   }, [])
 
   return (
-    <div className="products-page p-5">
+    <div className="products-page">
       <div className="text-center header-produtos d-flex align-items-end justify-content-center p-5">
-        <h1 className='wood-text header-titulo fw-bolder'>Escolha seu veneno</h1>
+        <h1 className='wood-text header-titulo fw-bolder mb-5'>Escolha seu veneno</h1>
       </div>
-      <h2 className="tituloProduto light-text" id="produtos">Cervejas</h2>
-      <div >
-        {cervejas ? (
+      <div className="products-list">
+        <h2 className="tituloProduto light-text" id="produtos">Cervejas</h2>
+        <div >
+          {cervejas ? (
 
-          <div className='content product-grid'>
+            <div className='content product-grid'>
 
-            {cervejas.map(cerveja => (
-              <div className="product-card light-text" key={cerveja.id}>
-                <div className="card-img">
-                  <img src={cerveja.imagem} alt="My Image" />
+              {cervejas.map(cerveja => (
+                <div className="product-card light-text" key={cerveja.id}>
+                  <div className="card-img">
+                    <img src={cerveja.imagem} alt="My Image" />
+                  </div>
+                  <div>
+                    <h3>{cerveja.nome}</h3>
+                    <p>Unidade: {cerveja.unidade}</p>
+                    <p className="price">Preço: {cerveja.preco}</p>
+                    <button>Adicionar ao carrinho</button>
+                  </div>
                 </div>
-                <div>
-                  <h3>{cerveja.nome}</h3>
-                  <p>Unidade: {cerveja.unidade}</p>
-                  <p className="price">Preço: {cerveja.preco}</p>
-                  <button>Adicionar ao carrinho</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>Carregando...</div>
-        )}
-      </div>
-      <h2 className="tituloProduto light-text">Destilados</h2>
-      <div >
-        {destilados ? (
+              ))}
+            </div>
+          ) : (
+            <div>Carregando...</div>
+          )}
+        </div>
+        <h2 className="tituloProduto light-text">Destilados</h2>
+        <div >
+          {destilados ? (
 
-          <div className='content product-grid'>
+            <div className='content product-grid'>
 
-            {destilados.map(destilado => (
-              <div className="product-card light-text" key={destilado.id}>
-                <div className="card-img">
-                  <img src={destilado.imagem} alt="My Image" />
+              {destilados.map(destilado => (
+                <div className="product-card light-text" key={destilado.id}>
+                  <div className="card-img">
+                    <img src={destilado.imagem} alt="My Image" />
+                  </div>
+                  <div>
+                    <h3>{destilado.nome}</h3>
+                    <p>Unidade: {destilado.unidade}</p>
+                    <p className="price">Preço: {destilado.preco}</p>
+                    <button>Adicionar ao carrinho</button>
+                  </div>
                 </div>
-                <div>
-                  <h3>{destilado.nome}</h3>
-                  <p>Unidade: {destilado.unidade}</p>
-                  <p className="price">Preço: {destilado.preco}</p>
-                  <button>Adicionar ao carrinho</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>Carregando...</div>
-        )}
-      </div>
-      <h2 className="tituloProduto light-text">Drinques</h2>
-      <div >
-        {coqueteis ? (
+              ))}
+            </div>
+          ) : (
+            <div>Carregando...</div>
+          )}
+        </div>
+        <h2 className="tituloProduto light-text">Drinques</h2>
+        <div >
+          {coqueteis ? (
 
-          <div className='content product-grid'>
+            <div className='content product-grid'>
 
-            {coqueteis.map(coquetel => (
-              <div className="product-card light-text" key={coquetel.id}>
-                <div className="card-img">
-                  <img src={coquetel.imagem} alt="My Image" />
+              {coqueteis.map(coquetel => (
+                <div className="product-card light-text" key={coquetel.id}>
+                  <div className="card-img">
+                    <img src={coquetel.imagem} alt="My Image" />
+                  </div>
+                  <div>
+                    <h3>{coquetel.nome}</h3>
+                    <p>Unidade: {coquetel.unidade}</p>
+                    <p className="price">Preço: {coquetel.preco}</p>
+                    <button>Adicionar ao carrinho</button>
+                  </div>
                 </div>
-                <div>
-                  <h3>{coquetel.nome}</h3>
-                  <p>Unidade: {coquetel.unidade}</p>
-                  <p className="price">Preço: {coquetel.preco}</p>
-                  <button>Adicionar ao carrinho</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>Carregando...</div>
-        )}
-      </div>
-    </div >
+              ))}
+            </div>
+          ) : (
+            <div>Carregando...</div>
+          )}
+        </div>
+      </div >
+    </div>
   );
 }
 
